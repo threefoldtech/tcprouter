9fd3bbc0ee391db1ce0ced29dee851e425574a5e
xmonader <xmonader@gmail.com>
2019-06-09 14:42:40 +0200

    wip

--- main.go
+++ main.go
@@ -5,12 +5,24 @@ import (
 	"bytes"
 	"crypto/tls"
 	"fmt"
-	"github.com/coredns/coredns/plugin/pkg/log"
 	"io"
+	"log"
 	"net"
+	"strconv"
 	"strings"
 	"sync"
+	"time"
+
+	"github.com/abronan/valkeyrie"
+	"github.com/abronan/valkeyrie/store"
+	"github.com/abronan/valkeyrie/store/redis"
 )
+
+func init() {
+
+	redis.Register()
+}
+
 type Backend struct {
 	Addr string
 	Port int
@@ -23,16 +35,19 @@ type ServerOptions struct {
 
 type Server struct {
 	ServerOptions ServerOptions
+	DbStore       store.Store
 	Backends      map[string]Backend
-	backendM 	  sync.RWMutex
+	backendM      sync.RWMutex
 }
+
 func NewServer(forwardOptions ServerOptions) *Server {
 	return &Server{ServerOptions: forwardOptions}
 }
 
 func (s *Server) RegisterBackend(name, remoteAddr string, port int) {
+	fmt.Println("register ", name, remoteAddr, port)
 	s.backendM.Lock()
-	s.Backends[name] = Backend{Addr:remoteAddr, Port:port}
+	s.Backends[name] = Backend{Addr: remoteAddr, Port: port}
 	s.backendM.Unlock()
 }
 
@@ -42,34 +57,86 @@ func (s *Server) DeleteBackend(name string) {
 	s.backendM.Unlock()
 
 }
+func (s *Server) monitorDbForBackends() {
+
+	for {
+
+		backendPairs, err := s.DbStore.List("router/register/", nil)
+		// fmt.fmt.Println("backendPairs", backendPairs, " err: ", err)
+		fmt.Println(err)
+		fmt.Println(len(backendPairs))
+		for _, backendPair := range backendPairs {
+			backendName := string(backendPair.Value)
+			fmt.Println("backedname :", backendName)
+			sniKey := fmt.Sprintf("router/backend/%s/sni", backendName)
+			addrKey := fmt.Sprintf("router/backend/%s/addr", backendName)
+			fmt.Println("sni key: ", sniKey)
+			fmt.Println("addr key: ", addrKey)
+
+			backendSNI, _ := s.DbStore.Get(sniKey, nil)
+			fmt.Println(string(backendSNI.Value), err)
+
+			backendAddr, _ := s.DbStore.Get(addrKey, nil)
+			fmt.Println("*****backendAddr: ", backendAddr.Value)
+			parts := strings.Split(string(backendAddr.Value), ":")
+			addr, portStr := parts[0], parts[1]
+			port, _ := strconv.Atoi(portStr)
+			s.RegisterBackend(string(backendSNI.Value), addr, port)
+
+		}
+		time.Sleep(time.Second * 3)
+	}
+
+}
 
 func (s *Server) Start() {
+
+	s.DbStore.Put("router/register/server1", []byte("server1"), nil)
+	s.DbStore.Put("router/backend/server1/sni", []byte("server1.com"), nil)
+	s.DbStore.Put("router/backend/server1/addr", []byte("127.0.0.1:5500"), nil)
+
+	go s.monitorDbForBackends()
+
 	var ln net.Listener
-	var err	error
+	var err error
 	ln, err = net.Listen("tcp", fmt.Sprintf("%s:%d", s.ServerOptions.listeningAddr, s.ServerOptions.listeningPort))
 
 	if err != nil {
-		println("err: ", err)
+		fmt.Println("err: ", err)
 		// handle error
 	}
 
-	println("Started server..")
+	fmt.Println("Started server..")
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			println("err")
+			fmt.Println("err")
 			// handle error
 		}
 		go s.handleConnection(conn)
 	}
 }
 func main() {
-	serverOpts := ServerOptions{listeningAddr:"0.0.0.0", listeningPort:443}
+
+	// Initialize a new store with consul
+	kv, err := valkeyrie.NewStore(
+		store.REDIS,
+		[]string{"127.0.0.1:6379"},
+		&store.Config{
+			ConnectionTimeout: 10 * time.Second,
+		},
+	)
+	if err != nil {
+		log.Fatal("Cannot create store redis")
+	}
+
+	serverOpts := ServerOptions{listeningAddr: "0.0.0.0", listeningPort: 443}
 	s := NewServer(serverOpts)
+	s.DbStore = kv
 	s.Backends = make(map[string]Backend)
 	// let's make it configurable later.
-	s.Backends["*"] = Backend{Addr:"127.0.0.1", Port:9092}
-	s.Backends["first.mybot.testsbots.grid.tf"] = Backend{Addr:"37.59.44.168", Port:443}
+	s.Backends["*"] = Backend{Addr: "127.0.0.1", Port: 9092}
+	s.Backends["first.mybot.testsbots.grid.tf"] = Backend{Addr: "37.59.44.168", Port: 443}
 	s.Start()
 
 }
@@ -83,10 +150,10 @@ func GetConn(conn net.Conn, peeked string) net.Conn {
 	}
 	return conn
 }
-func (s *Server) handleConnection(mainconn net.Conn) (error) {
+func (s *Server) handleConnection(mainconn net.Conn) error {
 	br := bufio.NewReader(mainconn)
 	serverName, isTls, peeked := clientHelloServerName(br)
-	println("SERVER NAME: ", serverName, " isTLS: ", isTls)
+	fmt.Println("SERVER NAME: ", serverName, " isTLS: ", isTls)
 
 	conn := GetConn(mainconn, peeked)
 	serverName = strings.ToLower(serverName)
@@ -99,23 +166,23 @@ func (s *Server) handleConnection(mainconn net.Conn) (error) {
 			s.backendM.Unlock()
 			return fmt.Errorf("backend doesn't exist: %s and no '*' backend for request.", backend)
 
-		} else{
-			println("using global catchall backend.")
+		} else {
+			fmt.Println("using global catchall backend.")
 		}
 	}
 	s.backendM.Unlock()
 	// remoteAddr := fmt.Sprintf("%s:%d",s.ServerOptions.remoteAddr,  s.ServerOptions.remotePort)
-	remoteAddr := &net.TCPAddr{IP:net.ParseIP(backend.Addr), Port:backend.Port}
-	println("found backend: ", remoteAddr)
-	log.Debugf("Handling connection from %s", conn.RemoteAddr())
+	remoteAddr := &net.TCPAddr{IP: net.ParseIP(backend.Addr), Port: backend.Port}
+	fmt.Println("found backend: ", remoteAddr)
+	fmt.Println("Handling connection from %s", conn.RemoteAddr())
 	defer conn.Close()
 
 	connBackend, err := net.DialTCP("tcp", nil, remoteAddr)
 	if err != nil {
-		log.Errorf("Error while connection to backend: %v", err)
+		fmt.Println("Error while connection to backend: %v", err)
 		return err
-	}else{
-		println("connected to the backend...")
+	} else {
+		fmt.Println("connected to the backend...")
 	}
 	defer connBackend.Close()
 
@@ -125,7 +192,7 @@ func (s *Server) handleConnection(mainconn net.Conn) (error) {
 
 	err = <-errChan
 	if err != nil {
-		log.Errorf("Error during connection: %v", err)
+		fmt.Println("Error during connection: %v", err)
 		return err
 	}
 	return nil
@@ -165,7 +232,7 @@ func clientHelloServerName(br *bufio.Reader) (string, bool, string) {
 	hdr, err := br.Peek(1)
 	if err != nil {
 		if err != io.EOF {
-			log.Errorf("Error while Peeking first byte: %s", err)
+			fmt.Println("Error while Peeking first byte: %s", err)
 		}
 		return "", false, ""
 	}
@@ -178,13 +245,13 @@ func clientHelloServerName(br *bufio.Reader) (string, bool, string) {
 	const recordHeaderLen = 5
 	hdr, err = br.Peek(recordHeaderLen)
 	if err != nil {
-		log.Errorf("Error while Peeking hello: %s", err)
+		fmt.Println("Error while Peeking hello: %s", err)
 		return "", false, getPeeked(br)
 	}
 	recLen := int(hdr[3])<<8 | int(hdr[4]) // ignoring version in hdr[1:3]
 	helloBytes, err := br.Peek(recordHeaderLen + recLen)
 	if err != nil {
-		log.Errorf("Error while Hello: %s", err)
+		fmt.Println("Error while Hello: %s", err)
 		return "", true, getPeeked(br)
 	}
 	sni := ""
@@ -201,7 +268,7 @@ func clientHelloServerName(br *bufio.Reader) (string, bool, string) {
 func getPeeked(br *bufio.Reader) string {
 	peeked, err := br.Peek(br.Buffered())
 	if err != nil {
-		log.Errorf("Could not get anything: %s", err)
+		fmt.Println("Could not get anything: %s", err)
 		return ""
 	}
 	return string(peeked)
@@ -220,8 +287,7 @@ func (c sniSniffConn) Read(p []byte) (int, error) { return c.r.Read(p) }
 // Write crashes all the time
 func (sniSniffConn) Write(p []byte) (int, error) { return 0, io.EOF }
 
-
 func connCopy(dst, src net.Conn, errCh chan error) {
-	_ , err := io.Copy(dst, src)
+	_, err := io.Copy(dst, src)
 	errCh <- err
 }
